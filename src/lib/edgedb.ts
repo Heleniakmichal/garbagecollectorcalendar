967da1e70c94e26013bd8cded9fd0a76c5f7494e
hell2hell <heleniakmichal@gmail.com>
2025-05-24 21:22:50 +0200

    db connection

--- src/lib/edgedb.ts
+++ src/lib/edgedb.ts
@@ -1,5 +1,9 @@
 import { createClient } from 'edgedb';
 
+if (!process.env.EDGEDB_DSN) {
+  throw new Error('EDGEDB_DSN environment variable is not set');
+}
+
 const client = createClient({
   dsn: process.env.EDGEDB_DSN,
   tlsSecurity: 'insecure',
